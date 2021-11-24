7fe9af28af71a717f52b6fcf203b74ab3668f54b
rlawo0307 <rlawo0307@naver.com>
2021-11-24 15:25:30 +0900

    .

--- Deque.cpp
+++ Deque.cpp
@@ -10,8 +10,8 @@ void AddFront(int*, int*, int*, int);
 void AddRear(int*, int*, int*, int);
 void DelFront(int*, int*, int*);
 void DelRear(int*, int*, int*);
-void GetFront(int*, int*, int*);
-void GetRear(int*, int*, int*);
+void GetFront(int*, int);
+void GetRear(int*, int);
 void Display(int*, int, int);
 
 int main()
@@ -20,13 +20,31 @@ int main()
 	int front = 0; 
 	int rear = 0; 
 
-	AddRear(dq, &front, &rear, 1);
-	AddRear(dq, &front, &rear, 2);
+	AddFront(dq, &front, &rear, 1);
+	AddFront(dq, &front, &rear, 2);
 	AddRear(dq, &front, &rear, 3);
-	printf("%d %d\n", front, rear);
-	AddFront(dq, &front, &rear, 4);
 	Display(dq, front, rear);
-	printf("%d %d\n", front, rear);
+
+	AddRear(dq, &front, &rear, 4);
+	AddRear(dq, &front, &rear, 5);
+	AddFront(dq, &front, &rear, 6);
+	AddRear(dq, &front, &rear, 7);
+	AddRear(dq, &front, &rear, 8); // Fail
+	Display(dq, front, rear);
+
+	DelFront(dq, &front, &rear);
+	Display(dq, front, rear);
+	DelRear(dq, &front, &rear);
+	Display(dq, front, rear);
+
+	AddFront(dq, &front, &rear, 8);
+	AddRear(dq, &front, &rear, 9);
+	AddRear(dq, &front, &rear, 10); // Fail
+	AddFront(dq, &front, &rear, 10); // Fail
+	Display(dq, front, rear);
+
+	GetFront(dq, front);
+	GetRear(dq, rear);
 }
 
 bool IsEmpty(int front, int rear)
@@ -50,9 +68,8 @@ void AddFront(int* dq, int* front, int* rear, int val)
 		printf("Queue is Full : %d\n", val);
 		return;
 	}
-	*front = (*front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; // 澜荐贸府
-	printf("%d\n", *front);
 	dq[*front] = val;
+	*front = (*front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; // 澜荐贸府
 }
 
 void AddRear(int* dq, int* front, int* rear, int val)
@@ -68,22 +85,34 @@ void AddRear(int* dq, int* front, int* rear, int val)
 
 void DelFront(int* dq, int* front, int* rear)
 {
-
+	if (IsEmpty(*front, *rear))
+	{
+		printf("Queue is Empty\n");
+		return;
+	}
+	*front = (*front + 1) % MAX_QUEUE_SIZE;
+	printf("Pop : %d\n", dq[*front]);
 }
 
 void DelRear(int* dq, int* front, int* rear)
 {
-
+	if (IsEmpty(*front, *rear))
+	{
+		printf("Queue is Empty\n");
+		return;
+	}
+	printf("Pop : %d\n", dq[*rear]);
+	*rear = (*rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
 }
 
-void GetFront(int* dq, int* front, int* rear)
+void GetFront(int* dq, int front)
 {
-
+	printf("front : %d\n", dq[front + 1]);
 }
 
-void GetRear(int* dq, int* front, int* rear)
+void GetRear(int* dq, int rear)
 {
-
+	printf("rear : %d\n", dq[rear]);
 }
 
 void Display(int* dq, int front, int rear)
